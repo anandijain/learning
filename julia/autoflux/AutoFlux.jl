ddb6efc65af7d7ec28dd49304af9281eb7603cda
anandijain <anandj@uchicago.edu>
2019-09-08 15:16:16 -0500

    includes and methods export

--- julia/autoflux/AutoFlux.jl
+++ julia/autoflux/AutoFlux.jl
@@ -9,5 +9,13 @@ AutoFlux is a set of tools for shaping and constructing simple models in Flux.
 
 
 # export 
+using Flux
 
+include("./shape.jl")
+include("./arch.jl")
+
+using .Shape
+export log_dims,
+	get_layers,
+	mlp
 end
