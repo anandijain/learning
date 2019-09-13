b5464d3ec247a0953e961449941c0423d836a675
anandijain <anandj@uchicago.edu>
2019-09-13 12:51:54 -0500

    basic torchaudio

--- julia/autoflux/arch.jl
+++ julia/autoflux/arch.jl
@@ -10,3 +10,22 @@ function mlp(input_dim=784, output_dim=10, factor=2)
     model(x) = foldl((x, m) -> m(x), layers, init = x)
     return model
 end
+
+function conv(dim=2, flat_in::Int=784, flat_out::Int=10; classify=true)
+    # todo test for inexact error
+    tups = []
+    try
+        dim_size = flat_in ^ (1 / dim)
+        delta = Int(size - flat_out)
+    catch exception
+        if isa(exception, InexactError)
+            return
+        end
+        push!((dim_size, ))
+    end
+
+    while  > flat_out
+
+
+
+function
