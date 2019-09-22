b71ad453165b8b7656ee611bbddb12d3be8dbb3a
anandijain <anandj@uchicago.edu>
2019-09-22 18:19:58 -0500

    blender

---
+++ blender/intro/add_transform.py
@@ -0,0 +1,16 @@
+import bpy
+
+#def add_transform(seq):
+#    cur_frame = bpy.context.scene.frame_current
+#    c = 7
+#    bpy.ops.sequencer.effect_strip_add(type="TRANSFORM", channel=(c+1), frame_start=cur_frame)
+
+def add_movie(fn='/home/sippycups/Videos/short_clips/projector.mp4'):
+    cur_frame = bpy.context.scene.frame_current
+    channel = 7
+    bpy.ops.sequencer.movie_strip_add(filepath=fn, frame_start=cur_frame, channel=channel)
+    
+    
+if __name__ == "__main__":
+    add_movie()
+    
