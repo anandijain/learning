f13d615a531e77d0dd4660a879848fc53ec60a68
anandijain <anandj@uchicago.edu>
2019-11-27 19:03:46 -0600

    cleanup

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,11 +1,15 @@
 module Main exposing (..)
 
 import Browser
+import Browser exposing (Document, UrlRequest)
+import Browser.Navigation as Nav
+
 import Http
-import Html.Events exposing (onClick)
+
 import Html exposing (..)
 import Html.Attributes exposing(..)
-import Random
+import Html.Events exposing (onClick)
+import Url exposing(Url)
 
 import Json.Decode exposing (Decoder, field, string, index, decodeString)
 
@@ -14,6 +18,9 @@ import Competition exposing(Competition, decodeCompetition, encodeCompetition)
 {-
 TODO:
   - format json into tables
+    - String -> Competition using Json.Decode
+    - mimic bov.parse_event to take List Event into row :: List String
+    - use cases to serialize row :: List String -> List Maybe Number
   - route to game_id endpoints
 -}
 
@@ -21,44 +28,58 @@ TODO:
 -- MAIN
 
 
-document =
-  Browser.element
+main : Program () Model Msg
+main =
+  application
     { init = init
+    , view = view
     , update = update
     , subscriptions = subscriptions
-    , view = view
+    , onUrlChange = UrlChanged
+    , onUrlRequest = LinkClicked
     }
 
 
 
 -- MODEL
-type Model
-  = Failure
-  | Loading
-  | Success String  -- suffix
 
 
-init : () -> (Model, Cmd Msg)
-init _ =
-  (Loading, getSport)
-
-
-type alias SportModel =
-  { sport : Sport
+type alias Model =
+  { key : Nav.Key
+  , url : Url.Url
   }
+
+
+-- init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+-- init flags url key =
+--   ( Model key url, Cmd.none )
+
+
+init : Maybe Viewer -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init maybeViewer url navKey =
+    changeRouteTo (Route.fromUrl url)
+        (Redirect (Session.fromViewer navKey maybeViewer))
+
+
 
 -- UPDATE
 
 
+-- type Msg
+--   = NextSport
+--   | GotJson (Result Http.Error String)
+
+
 type Msg
-  = NextSport
-  | GotJson (Result Http.Error String)
+    = ChangedUrl Url
+    | ClickedLink UrlRequest
+    | GotHomeMsg Home.Msg
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    nextSport ->
+    NextSport ->
       (Loading, getSport)
 
     GotJson result ->
@@ -69,19 +90,6 @@ update msg model =
         Err _ ->
           (Failure, Cmd.none)
 
-
-sportGenerator : Random.Generator
-sportGenerator =
-  Random.uniform "basketball/nba"
-    [ "baseball/mlb"
-    , "football/nfl"
-    , "hockey/nhl"
-    , "esports"
-    , "tennis"
-    , "volleyball"
-    , "badminton"
-    , "table-tennis"
-    ]
 
 -- SUBSCRIPTIONS
 
@@ -94,44 +102,64 @@ subscriptions model =
 
 -- VIEW
 
-
-view : Model -> Html Msg
+view : Model -> Document Msg
 view model =
-  div []
-    [ h2 [] [ text "Moneylines" ]
-    , viewSport model
-    ]
+  { title = "URL Interceptor"
+  , body =
+      [ text "The current URL is: "
+      , b [] [ text (Url.toString model.url) ]
+      , ul []
+          [ viewLink "/home"
+          , viewLink "/index.html"
+          , viewLink "/nba"
+          , viewLink "/nfl"
+          , viewLink "/nhl"
+          ]
+      ]
+  }
 
 
-viewSport : Model -> Html Msg
-viewSport model =
-  case model.status of
-    Failure ->
-      div []
-        [ text "could not load"
-        , button [ onClick getSport ] [ text "next sport" ]
-        ]
-    Loading ->
-      text "Loading..."
+viewLink : String -> Html msg
+viewLink path =
+  li [] [ a [ href path ] [ text path ] ]
 
-    Success fullJson -> onSuccess fullJson
+-- view : Model -> Html Msg
+-- view model =
+--   div []
+--     [ h2 [] [ text "Moneylines" ]
+--     , viewSport model
+--     ]
+
+
+
+-- viewSport : Model -> Html Msg
+-- viewSport model =
+--   case model of
+--     Failure ->
+--       div []
+--         [ text "could not load"
+--         , button [ onClick NextSport ] [ text "next sport" ]
+--         ]
+--     Loading ->
+--       text "Loading..."
+
+--     Success fullJson -> onSuccess fullJson
 
 
 onSuccess : String -> Html Msg
 onSuccess textData =
   div []
-    [ button [ onClick nextSport ] [ text "next sport" ]
-    , button [ onClick nextSport ] [ text textData ]
+    [ button [ onClick NextSport ] [ text "next sport" ]
+    , button [ onClick NextSport ] [ text textData ]
     ]
 
 
 -- HTTP
 
-
 getSport : Cmd Msg
 getSport = 
   Http.get
-    { url = "https://www.bovada.lv/services/sports/event/v2/events/A/description/"
+    { url = "https://www.bovada.lv/services/sports/event/v2/events/A/description/basketball/nba"
     , expect = Http.expectString GotJson
     }
 
