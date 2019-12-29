38a79265c53c5892f5bf3fa5721a70d72e414f35
anand <anandj@uchicago.edu>
2019-12-29 14:45:00 -0800

    starting to learn cpp

---
+++ cpp/proj/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+// #include "side.h"
+
+using namespace std;
+
+class Person{
+    public:
+        string name;
+        string major;
+        float gpa;
+
+        Person(string aName, string aMajor, float aGPA){
+            name = aName;
+            major = aMajor;
+            gpa = aGPA;
+        }
+};
+
+
+string writePerson(Person p, bool printPerson)
+{
+    // printName ? (cout << name << endl) : ;
+
+    if (printPerson)
+    {
+        cout << p.name << endl;
+        cout << p.major << endl;
+        cout << p.gpa << endl;
+    }
+    return p.name;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc % 3 != 1){
+
+        cout << argc << endl;
+        cout << "num of args must be multiple of 3 w remainder 1" << endl;
+        return 0;
+    }
+    // Person[] people;
+    bool tf = true;
+    for (int i = 3; i < argc; i+= 3){
+        
+        // i % 2 == 0 ? tf = true : tf = false;
+         
+        Person p = Person(argv[i-2], argv[i-1], std::stof(argv[i]));
+        
+        writePerson(p, tf);
+        if (i + 3 > argc){
+            break;
+        }
+        
+    }
+    return 0;
+}
+
