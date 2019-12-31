9697158e30d3e6b0b2b9155a8d9dc8727753b616
anand <anandj@uchicago.edu>
2019-12-30 17:59:23 -0800

    updates:

--- random/pythonstuff/fin/edgar.py
+++ random/pythonstuff/fin/edgar.py
@@ -5,17 +5,16 @@ import pandas as pd
 
 """
 list companies
-get company ids 
+get company ids
 """
 
 
-
-def search_link(name:str, form_type='13F-HR') -> str:
+def search_link(name: str, form_type='13F-HR') -> str:
     company = name.replace(' ', '+')
-    return 'https://www.sec.gov/cgi-bin/browse-edgar?company=' + company + '&type=' + form_type + '&count=100&output=xml'
+    return 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&company=' + company + '&type=' + form_type + '&count=100&output=xml'
 
 
-def get_page(link:str)->bs4.BeautifulSoup:
+def get_page(link: str) -> bs4.BeautifulSoup:
     return bs4.BeautifulSoup(r.get(link).text, 'html.parser')
 
 
@@ -28,51 +27,57 @@ def next_pages(page: bs4.BeautifulSoup) -> list:
         next_page = page.find('span', {'id': 'next'})
 
     return pages
-        
 
-def grab_docs_links(page:bs4.BeautifulSoup) -> list:
+
+def grab_docs_links(page: bs4.BeautifulSoup) -> list:
     all_links = []
     all_pages = next_pages(page)
     for page in all_pages:
-        t = page.find('table', {'class' : 'results'})
+        t = page.find('table', {'class': 'results'})
         links = [l['href'] for l in t.find_all('a')]
         all_links += links
 
-    #  = [l['href'] for l in page.find_all('a', {'id' : 'documentsbutton'}) for page in all_pages]
     return all_links
-        
 
 
-def get_holdings(page:bs4.BeautifulSoup, output='df') -> pd.DataFrame:
+def get_holdings(page: bs4.BeautifulSoup, output='df') -> pd.DataFrame:
     table = page.find('table', {'summary': 'Form 13F-NT Header Information'})
 
     if output == 'df':
         ret = clean_holdings(pd.read_html(table.prettify())[0])
-        
+
     return ret
 
-def clean_holdings(df:pd.DataFrame) -> pd.DataFrame:
+
+def clean_holdings(df: pd.DataFrame) -> pd.DataFrame:
     df.columns = df.iloc[2]
     df.drop([0, 1, 2], inplace=True)
     return df
+
 
 def get_CIKs():
     cik = 'https://www.sec.gov/cgi-bin/cik_lookup'
     return cik
 
-if __name__ == "__main__":
-    
-    # for maximal CIK lookup give reduced names, eg 'Two Sigma' instead of 'Two Sigma Investments' 
-    companies = ['Renaissance Technologies', 'Two Sigma Investments', 'Bridgewater Associates',
-        'AQR Capital Management', 'Millennium Management', 'Elliott Management', 'BlackRock', 'Citadel LLC']
 
+def main():
+    """
+    1. given list of companies, gather all of the CIKs using cik_lookup
+    """
+
+    companies = ['Renaissance Technologies', 'Two Sigma Investments', 'Bridgewater Associates',
+                    'AQR Capital Management', 'Millennium Management', 'Elliott Management', 'BlackRock', 'Citadel LLC']
     links = list(map(search_link, companies))
     pages = list(map(get_page, links))
-    document_links = list(map(get_pages, pages)
-    dfs = list(map(get_holdings, pages))
+    document_links = list(map(grab_docs_links, pages))
+    doc_pages = list(map(get_page, document_links))
+
+    dfs = list(map(get_holdings, doc_pages))
 
     print(dfs)
-    # print(links)
-    # print(len(pages))
+    return dfs
 
+if __name__ == "__main__":
 
+    dfs = main()
+    print(dfs)
