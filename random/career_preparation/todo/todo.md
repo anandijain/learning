21a01d75bef4547c432b93c727d2bbef1f85c971
anandijain <anandj@uchicago.edu>
2020-03-12 21:55:47 -0500

    update

---
+++ random/career_preparation/todo/todo.md
@@ -0,0 +1,5 @@
+todo
+
+completed
+
+https://chp.tbe.taleo.net/chp02/ats/careers/v2/viewRequisition?org=AMBARELLA2015&cws=50&rid=745
---
+++ rust/hello.rs
@@ -0,0 +1,5 @@
+fn main() {
+    /* Block comments which go to the closing delimiter. */
+    // https://transform.tools/json-to-rust-serde
+    println!("Hello World!");
+}
